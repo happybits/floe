fc7dd2a5da24c4e99809ee23790f598b3c5ab48c
Danny Maxson <danny@happybits.co>
2019-05-22 12:56:23 +0200

    [fix] change to response streams to get the tests running

--- floe/restserver.py
+++ floe/restserver.py
@@ -24,7 +24,8 @@ class RestServerFloeIndex(object):
         # not setting because wsgiref doesn't natively support it and
         # uwsgi will automatically handle this for us.
         # resp.append_header('Transfer-Encoding', 'chunked')
-        resp.set_stream(generator(), stream_len=None)
+        resp.stream = generator()
+        # resp.set_stream(generator(), stream_len=None)
 
     @app_trace
     def on_delete(self, req, resp, domain):
