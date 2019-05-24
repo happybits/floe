c5a95859730ff91566259fee3ed4e72119dc4180
Danny Maxson <danny@happybits.co>
2019-05-24 14:31:24 +0200

    [python3] revert back shell autoformatting

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-command -v virtualenv >/dev/null 2>&1 || {
-    echo >&2 "I require virtualenv but it's not installed.  Aborting."
-    exit 1
-}
+command -v virtualenv >/dev/null 2>&1 || { echo >&2 "I require virtualenv but it's not installed.  Aborting."; exit 1; }
 
 root_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 
 os=$(uname -s)
 os_hardware=$(uname -m)
@@ -13,27 +11,30 @@ venv_dir="$root_dir/.venv-$os-$os_hardware"
 
 # make sure we have mysqld
 command -v mysqld >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    if [ "$os" == "Darwin" ]; then
-        echo >&2 "mysqld missing - installing ..."
+if [ $? -ne 0 ]
+then
+    if [ "$os" == "Darwin" ]
+    then
+        >&2 echo "mysqld missing - installing ..."
         brew update && brew install mysql || exit 1
     else
-        echo >&2 "mysqld missing - please install"
+        >&2 echo "mysqld missing - please install"
         exit 1
     fi
 fi
 
-if [ ! -f "$venv_dir/bin/python" ]; then
+
+if [ ! -f "$venv_dir/bin/python" ]
+then
     echo "configuring virtualenv $venv_dir ..."
-    virtualenv -p python3 -q "$venv_dir" || {
-        echo >&2 "unable to configure the virtualenv for the project in $venv_dir"
-        exit 1
-    }
+    virtualenv -p python3 -q "$venv_dir" || { echo >&2 "unable to configure the virtualenv for the project in $venv_dir"; exit 1; }
 fi
+
 
 "$venv_dir/bin/pip" install -q -r "$root_dir/dev-requirements.txt"
 
-if [ $? -ne 0 ]; then
-    echo >&2 "failed to install pip packages"
+if [ $? -ne 0 ]
+then
+    >&2 echo "failed to install pip packages"
     exit 1
 fi
